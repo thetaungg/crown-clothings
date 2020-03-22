ca213fa37c32a62b3924413094e9641fd228ff77
Maxwell <thetaung@nuzay.com>
2020-03-22 11:35:51 +0630

    remove redux from directory section

--- client/src/components/directory/directory.component.jsx
+++ client/src/components/directory/directory.component.jsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import './directory.styles.scss'
 import MenuItem from "../menu-item/menu-item.component";
-
-import { connect } from 'react-redux';
-import { createStructuredSelector } from "reselect";
-import { selectDirectorySections } from "../../redux/directory/directory.selectors";
+import DirectoryData from './directory-data'
 
 const Directory = ({sections}) => {
       return(
       <div className="directory-menu">
           {
-             sections.map(({id,...otherSectionProps}) =>  //es6 property
+             DirectoryData.map(({id,...otherSectionProps}) =>  //es6 property
                 <MenuItem key={id} {...otherSectionProps}/> //we can do it like this because the names of both sides of equations are the same eg size={size}
                 )
           }
       </div>
 )};
 
-const mapStateToProps = createStructuredSelector({
-      sections: selectDirectorySections
-});
-
-export default connect(mapStateToProps)(Directory);
+export default Directory;

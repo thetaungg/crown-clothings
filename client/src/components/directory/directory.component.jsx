3a71cf1f1777445b70de36ac0bea5f7a0e5e5c3c
Maxwell <thetaung@nuzay.com>
2020-03-22 18:31:24 +0630

    fixing deployment bugs

--- client/src/components/directory/directory.component.jsx
+++ client/src/components/directory/directory.component.jsx
@@ -6,19 +6,19 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from "reselect";
 import { selectDirectorySections } from "../../redux/directory/directory.selectors";
 
-const Directory = ({sections}) => {
-      return(
-      <div className="directory-menu">
-          {
-             sections.map(({id,...otherSectionProps}) =>  //es6 property
-                <MenuItem key={id} {...otherSectionProps}/> //we can do it like this because the names of both sides of equations are the same eg size={size}
-                )
-          }
-      </div>
-)};
+const Directory = ({sections}) => (
+    <div className="directory-menu">
+        {       //es6 property
+            sections.map(({id,...otherSectionProps}) => (
+                <MenuItem key={id} {...otherSectionProps}/>
+                )//we can do it like this because the names of both sides of equations are the same eg size={size}
+            )
+        }
+    </div>
+);
 
 const mapStateToProps = createStructuredSelector({
-      sections: selectDirectorySections
+    sections: selectDirectorySections
 });
 
 export default connect(mapStateToProps)(Directory);
